cde4a673b8c793a0e979f33c168e2374f5b1d9ce
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-11-15 15:30:37 -0300

    feat: lint--staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
     // es2021: true,
     es6: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -18,7 +14,26 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    radix: 'off',
+    //'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js', '.ts', '.tsx'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'no-unused-expressions': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+  },
   settings: {
     'import/resolver': {
       node: {
@@ -26,4 +41,4 @@ module.exports = {
       },
     },
   },
-};
+}
